e79f7329e89cb7222f39f6f65ad755a10c1e4333
ShubhamUjinwal <shubhamujinwal114@gmail.com>
2020-06-27 17:10:25 -0400

    Updates

---
+++ precache-manifest.54a141cff15eec919a2ae76bd6b07dbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ac379bc5647b0930e74fd6fe9be86c",
+    "url": "/NextConnect/index.html"
+  },
+  {
+    "revision": "53a3d8417abf693eb2fd",
+    "url": "/NextConnect/static/css/main.aec92e74.chunk.css"
+  },
+  {
+    "revision": "60a089ce03007a02e8ff",
+    "url": "/NextConnect/static/js/2.4624fe4a.chunk.js"
+  },
+  {
+    "revision": "f4e523b1d7cb6fafdc93ff1d89c31731",
+    "url": "/NextConnect/static/js/2.4624fe4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a3d8417abf693eb2fd",
+    "url": "/NextConnect/static/js/main.5c7ec379.chunk.js"
+  },
+  {
+    "revision": "b6656369839374e70194",
+    "url": "/NextConnect/static/js/runtime-main.e95dbf7a.js"
+  },
+  {
+    "revision": "3bbe74fbc7285ce49c9825ec683c18fa",
+    "url": "/NextConnect/static/media/close.3bbe74fb.svg"
+  },
+  {
+    "revision": "7d7c0ba2e6cdbb43e6fdc5d8aa10f8f5",
+    "url": "/NextConnect/static/media/logout.7d7c0ba2.svg"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/NextConnect/static/media/user.7a21cbe9.svg"
+  }
+]);
